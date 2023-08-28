b6efd6585c12b714634e1f037bfca94f6530bf94
magda-3008 <138937286+magda-3008@users.noreply.github.com>
2023-08-28 16:41:58 -0600

    poco texto

--- ControldeCodigo/Program.cs
+++ ControldeCodigo/Program.cs
@@ -1,13 +1,3 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hola, estoy probando Github");
-Console.WriteLine("Hola, estoy probando Github");
-Console.WriteLine("Hola, estoy probando Github");
-Console.WriteLine("Hola, estoy probando Github");
-Console.WriteLine("Hola, estoy probando Github");
-Console.WriteLine("Hola, estoy probando Github");
-Console.WriteLine("Hola, estoy probando Github");
-Console.WriteLine("Hola, estoy probando Github");
-Console.WriteLine("Hola, estoy probando Github");
-Console.WriteLine("Hola, estoy probando Github");
-Console.WriteLine("Hola, estoy probando Github");
-Console.WriteLine("Hola, estoy probando Github");
+
