886be68cf6667059b2958188ffc8c5e77af7074f
Magda Castellón <magdaverocas@gmail.com>
2023-08-28 16:40:33 -0600

    hay demasiados mensajes xd

--- ControldeCodigo/Program.cs
+++ ControldeCodigo/Program.cs
@@ -1,3 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hola, estoy probando Github");
-Console.WriteLine();
+Console.WriteLine("Hola, estoy probando Github");
+Console.WriteLine("Hola, estoy probando Github");
+Console.WriteLine("Hola, estoy probando Github");
+Console.WriteLine("Hola, estoy probando Github");
+Console.WriteLine("Hola, estoy probando Github");
